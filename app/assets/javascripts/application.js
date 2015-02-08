7a95c349b53f6f83368e5fb0ef190dd3762b0033
Ripan <er.ripankumar@gmail.com>
2015-02-08 22:53:29 +0000

    Fix assets bootstrap issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require angular-route/angular-route
 //= require angular-resource/angular-resource
 //= require angular-rails-templates
-//= require bootstrap
+//= require bootstrap-sass-official
 //= require ng-underscore
 //= require_tree .
 
